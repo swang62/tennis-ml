b57ca41678263c16bca813df12b56afd9a02a8a9
Steve Wang <stevew77@gmail.com>
2026-08-01 20:51:39 +0800

    chore: update local k3d pref infra

--- infra/k3d/start.sh
+++ infra/k3d/start.sh
@@ -3,17 +3,11 @@ set -euo pipefail
 
 CLUSTER_NAME="tennis-ml"
 
-MACHINE_ID="infra/k3d/machine-id"
-if [ ! -f "$MACHINE_ID" ]; then
-  echo -n "abc123abc123abc123abc123abc123ab" > "$MACHINE_ID"
-fi
-
 if k3d cluster list | grep -q "$CLUSTER_NAME"; then
   echo "Cluster '$CLUSTER_NAME' already exists. Skipping..."
 else
   echo "Creating cluster '$CLUSTER_NAME'..."
-  k3d cluster create --config infra/k3d/config.yaml \
-    --volume "$PWD/$MACHINE_ID:/etc/machine-id"
+  k3d cluster create --config infra/k3d/config.yaml
 fi
 
 kubectl cluster-info
